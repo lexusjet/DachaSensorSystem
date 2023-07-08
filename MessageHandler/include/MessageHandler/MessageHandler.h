45a0858ec9f360cf8db0c7219b5b6f0a4ef8796c
Alexey lexusjet <lexusx6@gmail.com>
2023-07-08 03:24:29 +0300

    Удалены все функции, класс должен быть переработан

--- MessageHandler/include/MessageHandler/MessageHandler.h
+++ MessageHandler/include/MessageHandler/MessageHandler.h
@@ -1,10 +1,7 @@
 #pragma once
 
 #include <functional>
-#include <mutex>
 #include <SensorMessage/SensorMessage.h>
-#include <SensorMessageConverter/SensorMessageConverter.h>
-#include <SensorMessageValidator/SensorMessageValidator.h>
 
 // =============================================================================
 
@@ -15,23 +12,17 @@ class MessageHandler
     // возможно нужен колбек на действие если все прошло хорошо
 
 private:
-    SensorMessageValidator m_validator;
-    SensorMessageConverter m_converter;
     ErrorCallBack m_onErrorCallBack;
     
 public:
     MessageHandler() = delete;
     MessageHandler(
-        const SensorMessageValidator&,
-        const SensorMessageConverter&,
         const ErrorCallBack&
     );
     ~MessageHandler();
 
 public:
-    void handleMessage(SensorMessage);
-    void setValidator(const SensorMessageValidator&);
-    void setConverter(const SensorMessageConverter&);
+    void handleMessage(const SensorMessage);
     void setErrorCallBack(const ErrorCallBack&);
 private:
     std::string convertSensorMessageToQuery(const SensorMessage&);
